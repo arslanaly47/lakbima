65e4ffaeeda7da2e7d980e9c5a704accbb389892
Arslan Ali <arslanaly47@gmail.com>
2016-10-17 21:30:20 +0500

    Add devise views and controllers.

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,8 @@
+//= require iCheck/icheck.min.js
+
+$(document).ready(function() {
+  $('.i-checks').iCheck({
+    checkboxClass: 'icheckbox_square-green',
+    radioClass: 'iradio_square-green',
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'home/index'
   get 'home/minor'
   root 'home#index'
