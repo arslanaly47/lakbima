b0f68534eff0c41df6af275fd2282d7fe1a81f86
Arslan Ali <arslanaly47@gmail.com>
2016-10-18 19:27:12 +0500

    Add routes for employees.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords" }
+  resources :employees
   get 'users/new' => 'users/manages#new'
   get 'home/index'
   get 'home/minor'
