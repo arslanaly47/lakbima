ec0e9402b5f8bcc0c566846716a6baab5bbc64b4
Arslan Ali <arslanaly47@gmail.com>
2016-10-24 01:15:02 +0500

    Add currency_symbol to AllowanceType model to avoid nil check.

--- app/models/allowance_type.rb
+++ app/models/allowance_type.rb
@@ -2,4 +2,10 @@ class AllowanceType < ApplicationRecord
   validates :name, :lump_sum_amount, presence: true
 
   belongs_to :currency
+
+  validates_associated :currency
+
+  def currency_symbol
+    currency && currency.symbol
+  end
 end
