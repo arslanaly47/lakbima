3a4151467a126df9da987e95715c6774039c9366
Arslan Ali <arslanaly47@gmail.com>
2016-10-27 01:32:11 +0500

    Add uniqueness validation for Department and JobTitle models.

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,5 @@
 class Department < ApplicationRecord
   has_many :job_titles
   validates :name, :description, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/job_title.rb
+++ app/models/job_title.rb
@@ -1,6 +1,9 @@
 class JobTitle < ApplicationRecord
   belongs_to :department
 
+  validates :name, presence: true, uniqueness: true
+  validates_associated :department
+
   def department_name
     department.try(:name)
   end
