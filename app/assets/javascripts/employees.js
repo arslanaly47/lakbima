7c82e554686d9938cf381970ba64d4ef6722b224
Arslan Ali <arslanaly47@gmail.com>
2016-11-09 12:11:39 +0500

    Add blueimp gallery for individual objets on the show page for employees.

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -56,12 +56,14 @@ $(document).on('ready nested:fieldAdded', function() {
 });
 
 $(document).on('ready', function() {
-  document.getElementById('links').onclick = function(event) {
-    event = event || window.event;
-    var target = event.target || event.srcElement,
-      link  = target.src ? target.parentNode : target,
-      options = { index: link, event: event },
-      links = this.getElementsByTagName('a');
-    blueimp.Gallery(links, options);
-  };
+  $("div[id^='links-']").each(function(index) {
+    $(this).click(function(event) {
+      event = event || window.event;
+      var target = event.target || event.srcElement,
+        link  = target.src ? target.parentNode : target,
+        options = { index: link, event: event },
+        links = this.getElementsByTagName('a');
+      blueimp.Gallery(links, options);
+    });
+  });
 });
