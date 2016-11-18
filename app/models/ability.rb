c695b58c79cdeef5d1ec620a25b7f610356e43e9
Arslan Ali <arslanaly47@gmail.com>
2016-11-18 19:10:54 +0500

    Fix a bug regarding giving all the permissions to a single user.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user.role.permissions.each do |permission|
       if permission.subject_class.downcase == "all"
-        can permission.action.downcase.to_sym, permission.subject_class.to_sym
+        can permission.action.downcase.to_sym, permission.subject_class.downcase.to_sym
       elsif permission.action.downcase == "manage"
         can permission.action.downcase.to_sym, permission.subject_class.constantize
       elsif permission.action.downcase == "create"
