1a08ca0c8531544a57fce040cfe133a1ba863036
Arslan Ali <arslanaly47@gmail.com>
2016-12-06 23:13:41 +0500

    Add ASC order for accounts by Account ID, not the ID generated by database.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,8 @@
 class Account < ApplicationRecord
   validates :account_id, :name, :description, presence: true
 
+  default_scope { order('account_id ASC') }
+
   def self.available_accounts_for(journal_entry_id)
     journal_entry = JournalEntry.find(journal_entry_id)
     selected_account = find_by_id journal_entry.from_account_id
