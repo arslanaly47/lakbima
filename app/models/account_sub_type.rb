0ab3420ec38b7bbbef57068c30baccfa9104234b
Arslan Ali <arslanaly47@gmail.com>
2016-12-25 02:16:52 +0500

    Format the code in models.

--- app/models/account_sub_type.rb
+++ app/models/account_sub_type.rb
@@ -1,6 +1,6 @@
 class AccountSubType < ApplicationRecord
   has_many :account_types
-
   belongs_to :account_main_type
+
   validates :name, :description, :account_main_type, presence: true
 end
--- app/models/account_type.rb
+++ app/models/account_type.rb
@@ -1,9 +1,9 @@
 class AccountType < ApplicationRecord
-  belongs_to :account_sub_type
   has_one :account_main_type, through: :account_sub_type
   has_many :accounts
   has_many :dynamic_menus_from_account_types, inverse_of: :dynamic_menu
   has_many :from_dynamic_menus, through: :dynamic_menus_from_account_types, source: :dynamic_menu
+  belongs_to :account_sub_type
 
   validates :account_sub_type, :name, :description, presence: true
 end
