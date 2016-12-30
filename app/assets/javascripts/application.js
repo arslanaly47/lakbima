371f8cce8ddfe8ce6adac1798f5a6b1048777057
Arslan Ali <arslanaly47@gmail.com>
2016-12-30 22:24:14 +0500

    Add toastr notes instead of Bootstrap notifications sitewide.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,14 +31,20 @@
 $(document).ready(function () { 
   toastr.options = {
     "closeButton": true,
+    "debug": false,
     "progressBar": true,
+    "preventDuplicates": false,
+    "positionClass": "toast-top-right",
+    "onclick": null,
     "showDuration": "400",
     "hideDuration": "1000",
     "timeOut": "7000",
+    "extendedTimeOut": "1000",
     "showEasing": "swing",
+    "hideEasing": "linear",
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
-  };
+  }
 
   Pace.options.ajax.trackWebSockets = false;
   Pace.options.ajax                 = false;
