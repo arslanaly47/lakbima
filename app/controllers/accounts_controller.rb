2c4c00de0fada633de2d9c968128a4eccaaefe5e
Arslan Ali <arslanaly47@gmail.com>
2017-01-02 05:58:02 +0500

    Add the sortable functionality for chart of accounts.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,7 @@ class AccountsController < ApplicationController
   load_and_authorize_resource
 
   before_action :set_account, only: [:edit, :update, :show, :destroy, :job_titles]
+  helper_method :sort_column
 
   def new
     @account = Account.new
@@ -34,6 +35,7 @@ class AccountsController < ApplicationController
 
   def index
     @accounts = Account.includes(:account_sub_type).all
+    @accounts = Account.includes(:account_sub_type).order(sort_column + " " + sort_direction)
   end
 
   def destroy
@@ -53,5 +55,9 @@ class AccountsController < ApplicationController
 
   def set_account
     @account = Account.find(params[:id]) 
+  end
+
+  def sort_column
+    Account.column_names.include?(params[:sort])? params[:sort] : "id"
   end
 end
