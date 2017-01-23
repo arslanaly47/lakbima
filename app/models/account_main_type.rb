48c4e40dc47d1e56c174661cd3424a5162d1183d
Arslan Ali <arslanaly47@gmail.com>
2017-01-23 15:44:42 +0500

    Format the code in AccountMainType model.

--- app/models/account_main_type.rb
+++ app/models/account_main_type.rb
@@ -1,5 +1,5 @@
 class AccountMainType < ApplicationRecord
   has_many :account_sub_types
   has_many :account_types, through: :account_sub_types
-  has_many :accounts, through: :account_types
+  has_many :accounts,      through: :account_types
 end
