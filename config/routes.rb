b62373120620e89b0da669f94d062f1ed6c96480
Arslan Ali <arslanaly47@gmail.com>
2017-01-23 23:38:48 +0500

    Format config/routes.rb, and add a route for balance sheet.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       post 'deny'
     end
   end
+
   resources :departments do
     member do
       get 'job_titles'
@@ -35,26 +36,32 @@ Rails.application.routes.draw do
       post 'unterminate' => 'employees#unterminate'
     end
   end
+
   resources :users, only: [:edit] do
     collection do
       patch 'update_password'
       get   'check_uniqueness'
     end
   end
+
   resources :notifications, only: [:index] do
     member do
       post 'mark_as_read'
     end
   end
+
   resources :journal_entry_sessions, only: [:create] do
     resources :journal_entries, only: [:index, :create, :update, :destroy, :show]
   end
+
   resources :journal_entries, only: [:index] do
     collection do
       get :build_options
     end
   end
+
   delete 'journal_entry_sessions/:id/close' => 'journal_entry_sessions#close', as: :close_journal_entry_session
+
   resources :account_main_types, path: "account_headers", only: [:show, :edit, :update, :index] do
     member do
       get 'account_sub_headers'
@@ -63,18 +70,22 @@ Rails.application.routes.draw do
 
   resources :accounts do
     collection do
-      get 'view' => "accounts#view"
-      get 'account_tree' => "accounts#account_tree"
+      get 'view'          => 'accounts#view'
+      get 'account_tree'  => 'accounts#account_tree'
+      get 'balance_sheet' => 'accounts#balance_sheet'
     end
   end
+
   resources :account_sub_types, path: "account_sub_headers" do
     member do
       get 'account_lists'
     end
   end
+
   resources :account_types, path: "account_lists"
   get 'profile' => 'users#profile'
   patch 'update_profile' => 'users#update_profile'
+
   resources :dynamic_menus do
     collection do
       get 'to_account_type_ids/:from_account_type_ids' => 'dynamic_menus#to_account_type_ids'
