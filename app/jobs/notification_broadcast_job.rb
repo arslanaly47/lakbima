7c14ae56d966b1d698b7a6444af10ac4734853cb
IbraheemZ <ibraheem.zafar92@gmail.com>
2017-01-29 23:37:01 +0500

    full name in notice flash

--- app/jobs/notification_broadcast_job.rb
+++ app/jobs/notification_broadcast_job.rb
@@ -2,6 +2,7 @@ class NotificationBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(args)
-    ActionCable.server.broadcast 'activity_channel', message: nil, type: "Manager", notice: "Employee created a leave application"
+    user = User.find_by_id(args.user_id)
+    ActionCable.server.broadcast 'activity_channel', message: nil, type: "Manager", notice: "#{user.full_name} (Employee) applied for a #{args.number_of_days} #{(args.number_of_days > 1) ? 'days' : 'day'} leave" 
   end
 end
