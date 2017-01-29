5ad3f343db3fcab0d5d269e2f561c451cd52ef49
Arslan Ali <arslanaly47@gmail.com>
2017-01-30 03:36:09 +0500

    Use number of days in the message when a Leave Application is approved or denied.

--- app/jobs/update_broadcast_job.rb
+++ app/jobs/update_broadcast_job.rb
@@ -1,9 +1,10 @@
 class UpdateBroadcastJob < ApplicationJob
+  include ActionView::Helpers::TextHelper
   queue_as :default
 
   def perform(event)
     unless event.pending?
-      ActionCable.server.broadcast "activity_channel", message: event, type: "Manager", id: event.user_id, notice: "Your leave application has been #{event.status} ."
+      ActionCable.server.broadcast "activity_channel", message: event, type: "Manager", id: event.user_id, notice: "Your leave application for #{pluralize(event.number_of_days, "day")} has been #{event.status} ."
     end
   end
 end
