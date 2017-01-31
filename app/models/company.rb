fb5f727c0fd1125eb9bc965c95988ca9d4375a77
Arslan Ali <arslanaly47@gmail.com>
2017-02-01 01:13:20 +0500

    Add callback for creating a tenant when a company is created.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,11 @@
 class Company < ApplicationRecord
   validates :business_name, :address, :telephone, :subdomain, presence: true
   validates :subdomain, uniqueness: true
+
+  after_create :create_tenant
+
+  private
+  def create_tenant
+    Apartment::Tenant.create(subdomain)
+  end
 end
