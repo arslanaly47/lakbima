385314ee2de4478940f9690806663bdebd7e527a
Arslan Ali <arslanaly47@gmail.com>
2017-02-20 01:25:49 +0500

    Recover the edge cases in the migration.

--- db/migrate/20170214075857_remove_user_id_to_employees.rb
+++ db/migrate/20170214075857_remove_user_id_to_employees.rb
@@ -1,15 +1,16 @@
 class RemoveUserIdToEmployees < ActiveRecord::Migration[5.0]
   def up
+    remove_index :employees, :user_id
     ActiveRecord::Base.transaction do
       Employee.find_each do |employee|
-        user = employee.user
-        if user
+        if employee.respond_to?(:user_id) && employee.user_id
+          user = User.find(employee.user_id)
           user.employee_id = user.employee.id
           user.save!
         end
       end
     end
-    remove_reference :employees, :user
+    remove_column :employees, :user_id
   end
 
   def down
