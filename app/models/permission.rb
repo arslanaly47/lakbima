bdb1d96102eaf10a6dbb5258a273a90ad1182813
Arslan Ali <arslanaly47@gmail.com>
2017-03-13 22:50:57 +0500

    Add validation for subject_class and action for Permission model.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,7 +1,9 @@
 class Permission < ApplicationRecord
   has_and_belongs_to_many :roles
 
+  validates :subject_class, :action, presence: true
+
   def name
-    [action, subject_class]*' '
+    [action.capitalize, subject_class.capitalize]*' '
   end
 end
