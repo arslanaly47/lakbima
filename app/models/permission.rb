0d1ee054147ff4c8aa163133829c4680b88fb321
Arslan Ali <arslanaly47@gmail.com>
2017-03-26 02:13:16 +0500

    Add uniqueness check for subject_calss + action in Permission model.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,6 +2,7 @@ class Permission < ApplicationRecord
   has_and_belongs_to_many :roles
 
   validates :subject_class, :action, presence: true
+  validates :subject_class, uniqueness: { scope: :subject_class, message: "with this action already exists in the system." }
 
   def name
     [action.capitalize, subject_class.capitalize]*' '
