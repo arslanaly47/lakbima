32c4d14f7bba93bdaa8547067da1769e8837abb0
Arslan Ali <arslanaly47@gmail.com>
2017-03-29 04:43:28 +0500

    Fix a typo for validation in Permission model.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,7 +2,7 @@ class Permission < ApplicationRecord
   has_and_belongs_to_many :roles
 
   validates :subject_class, :action, presence: true
-  validates :subject_class, uniqueness: { scope: :subject_class, message: "with this action already exists in the system." }
+  validates :subject_class, uniqueness: { scope: :action, message: "with this action already exists in the system." }
 
   def name
     [action.capitalize, subject_class.capitalize]*' '
