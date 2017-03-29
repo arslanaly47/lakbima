5df61a9d212effcd54899a8c3d60be30a5d13c05
Arslan Ali <arslanaly47@gmail.com>
2017-03-30 02:57:42 +0500

    Modify the date format from %m/%d to %d/%m.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Date::DATE_FORMATS.merge! default: "%m/%d/%Y"
+Date::DATE_FORMATS.merge! default: "%d/%m/%Y"
