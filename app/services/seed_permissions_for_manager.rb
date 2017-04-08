20eefeeb3f568f8752d28d1bbd2a92260906d95f
Arslan Ali <arslanaly47@gmail.com>
2017-04-08 11:44:39 +0500

    Add extra permissions for accessing sub categories for an account type.

--- app/services/seed_permissions_for_manager.rb
+++ app/services/seed_permissions_for_manager.rb
@@ -16,6 +16,8 @@ class SeedPermissionsForManager
       end
     end
     Permission.find_or_create_by(subject_class: 'Employee', action: 'download_pdf')
+    Permission.find_or_create_by(subject_class: 'AccountMainType', action: 'account_sub_headers')
+    Permission.find_or_create_by(subject_class: 'AccountSubType', action: 'account_lists')
   end
 
   def seed_permissions_for_leave_application
