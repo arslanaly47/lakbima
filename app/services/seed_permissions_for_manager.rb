86d182fc11b135e1f5ae59cc6d791dde2ce3298d
Arslan Ali <arslanaly47@gmail.com>
2017-04-09 18:26:21 +0500

    Add permissions for viewing account tree, and balance sheet.

--- app/services/seed_permissions_for_manager.rb
+++ app/services/seed_permissions_for_manager.rb
@@ -18,6 +18,9 @@ class SeedPermissionsForManager
     Permission.find_or_create_by(subject_class: 'Employee', action: 'download_pdf')
     Permission.find_or_create_by(subject_class: 'AccountMainType', action: 'account_sub_headers')
     Permission.find_or_create_by(subject_class: 'AccountSubType', action: 'account_lists')
+    Permission.find_or_create_by(subject_class: 'Account', action: 'account_tree')
+    Permission.find_or_create_by(subject_class: 'Account', action: 'balance_sheet')
+    Permission.find_or_create_by(subject_class: 'Account', action: 'view')
   end
 
   def seed_permissions_for_leave_application
