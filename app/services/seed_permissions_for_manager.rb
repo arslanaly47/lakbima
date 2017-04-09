d54617d61572f15028aaccd6960a1adcec37b51c
Arslan Ali <arslanaly47@gmail.com>
2017-04-10 00:10:07 +0500

    Add permission for selecting the different accounts in TO section while creating a dynamic menu.

--- app/services/seed_permissions_for_manager.rb
+++ app/services/seed_permissions_for_manager.rb
@@ -21,6 +21,7 @@ class SeedPermissionsForManager
     Permission.find_or_create_by(subject_class: 'Account', action: 'account_tree')
     Permission.find_or_create_by(subject_class: 'Account', action: 'balance_sheet')
     Permission.find_or_create_by(subject_class: 'Account', action: 'view')
+    Permission.find_or_create_by(subject_class: 'DynamicMenu', action: 'to_account_type_ids')
   end
 
   def seed_permissions_for_leave_application
